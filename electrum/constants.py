7bc82ba7350dd99935bff6640ca340f2ff7c43fa
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2019-03-22 02:14:04 -0400

    match torba

--- electrum/constants.py
+++ electrum/constants.py
@@ -58,14 +58,14 @@ class BitcoinMainnet(AbstractNet):
     CHECKPOINTS = read_json('bullshit.json', [])
 
     XPRV_HEADERS = {
-        'standard':    0x019c3118,  # xprv
+        'standard':    0x0488ade4,  # xprv
         'p2wpkh-p2sh': 0x049d7878,  # yprv
         'p2wsh-p2sh':  0x0295b005,  # Yprv
         'p2wpkh':      0x04b2430c,  # zprv
         'p2wsh':       0x02aa7a99,  # Zprv
     }
     XPUB_HEADERS = {
-        'standard':    0x019c354f,  # xpub
+        'standard':    0x0488b21e,  # xpub
         'p2wpkh-p2sh': 0x049d7cb2,  # ypub
         'p2wsh-p2sh':  0x0295b43f,  # Ypub
         'p2wpkh':      0x04b24746,  # zpub
